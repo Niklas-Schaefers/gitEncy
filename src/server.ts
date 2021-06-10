3a95eb59fc9bb9ebf175458d64961e2c912bbbfb
Niklas Schäfers <Niklas.Schaefers@gmx.de>
2021-06-10 10:06:09 +0200

    🚑️ Forward client routing in production

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-
+import path from "path";
 import express from "express";
 
 const { PORT = 3000 } = process.env;
@@ -12,6 +12,11 @@ app.use("/storybook", express.static("dist/storybook"));
 
 // Serve app production bundle
 app.use(express.static("dist/app"));
+
+// Handle client routing, return all requests to the app
+app.get("*", (_req, res) => {
+  res.sendFile(path.join(__dirname, "app/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`photoplay app listening at http://localhost:${PORT}`);
