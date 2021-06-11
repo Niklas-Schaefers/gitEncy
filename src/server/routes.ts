65acdde98744d1d30e5a367b22c861a03945a611
Niklas Schäfers <Niklas.Schaefers@gmx.de>
2021-06-11 15:57:46 +0200

    ✅ test server/api fetch

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -6,16 +6,16 @@ const router = express.Router();
 router.get("/search", async (_req, res, next) => {
   try {
     const { query } = _req;
-    if (typeof query.code !== "string" || typeof query.user !== "string") {
-      res.status(400).send("Query is malformed");
-      return;
-    }
     if (!query.code) {
       res.status(400).send("Code required");
       return;
     }
     if (!query.user) {
       res.status(400).send("User required");
+      return;
+    }
+    if (typeof query.code !== "string" || typeof query.user !== "string") {
+      res.status(400).send("Query is malformed");
       return;
     }
     const result = await fetchGitHubSearchCodeWithUser(query.code, query.user);
