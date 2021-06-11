48c8fe3a2ee50c79dfe2ad4d8bb7cfe14c529281
Niklas Schäfers <Niklas.Schaefers@gmx.de>
2021-06-11 15:13:32 +0200

    ✨ add fetch function to routes

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,9 +1,27 @@
 import express from "express";
+import { fetchGitHubSearchCodeWithUser } from "./gitHubSearch";
 
 const router = express.Router();
 
-router.get("/search", (_req, res) => {
-  res.status(404).send("Hello World");
+router.get("/search", async (_req, res, next) => {
+  try {
+    const { query } = _req;
+    if (typeof query.code !== "string" || typeof query.user !== "string") {
+      res.status(400).send("Query is malformed");
+      return;
+    }
+    if (!query.code) {
+      res.status(400).send("Code required");
+      return;
+    }
+    if (!query.user) {
+      res.status(400).send("User required");
+      return;
+    }
+    const result = await fetchGitHubSearchCodeWithUser(query.code, query.user);
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
 });
-
 export default router;
