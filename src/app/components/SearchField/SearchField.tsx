0b17fa9008a44f8dfcfd94ef4e0f475e28a8be83
Niklas Schäfers <Niklas.Schaefers@gmx.de>
2021-06-14 11:18:02 +0200

    ➕ add searchfield for testing

---
+++ src/app/components/SearchField/SearchField.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./SearchField.module.css";
+import SearchFieldIcon from "../Icons/SearchFieldIcon";
+
+type InputProps = {
+  placeholder: string;
+  value: string;
+  onChange: (value: string) => void;
+};
+
+function SearchField({
+  placeholder,
+  value,
+  onChange,
+}: InputProps): JSX.Element {
+  return (
+    <label className={styles.label}>
+      <div className={styles.label__iconContainer}>
+        <SearchFieldIcon className={styles.label__icon} />
+      </div>
+      <input
+        className={styles.label__input}
+        placeholder={placeholder}
+        onChange={(event) => onChange(event.target.value)}
+        value={value}
+      />
+    </label>
+  );
+}
+
+export default SearchField;
