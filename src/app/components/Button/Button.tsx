5b3fe800f37214731b11f14730578b1ca11d2e0a
Niklas Schäfers <Niklas.Schaefers@gmx.de>
2021-06-14 15:55:54 +0200

    ➕ Add testing via jest

---
+++ src/app/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from "react";
+import styles from "./Button.module.css";
+
+type ButtonProps = {
+  children: ReactNode;
+};
+function Button({ children }: ButtonProps): JSX.Element {
+  return <button className={styles.button}>{children}</button>;
+}
+
+export default Button;
