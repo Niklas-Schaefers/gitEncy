04ec5248ee5cdc1d5acf2f1cab160bd3f1f93448
Niklas Schäfers <Niklas.Schaefers@gmx.de>
2021-06-15 12:09:25 +0200

    💄  change colors of icons and elements to var

--- src/app/components/Styling Elements/BubbleFooter.tsx
+++ src/app/components/Styling Elements/BubbleFooter.tsx
@@ -8,7 +8,7 @@ function BubbleFooter(props: React.SVGProps<SVGSVGElement>): JSX.Element {
         data-name="Intersection 2"
         d="M-407.939,124.238V45.683c15.474-.016,32.323.636,50.714,1.288,19.449.689,40.616,1.379,63.687,1.279,39.023-.17,56.849,1.383,72.912,2.937,18.082,1.748,33.931,3.5,75.269,2.788C-67.3,52.638-103.584,49.479,18.7,42.9l.36-.019v81.358Z"
         transform="translate(408.439 -42.353)"
-        fill="#14005a"
+        fill="var(--secondary-bg-color)"
         stroke-width="1"
       />
     </svg>
--- src/app/components/Styling Elements/BubbleHeader.tsx
+++ src/app/components/Styling Elements/BubbleHeader.tsx
@@ -12,7 +12,7 @@ function BubbleHeader(props: React.SVGProps<SVGSVGElement>): JSX.Element {
         data-name="Intersection 2"
         d="M-418.426,205.146V59.36h427v73.1c-21.679,17.759-40.951,30.453-40.951,30.453s-48.341,20.36-118.256,33.018c-47.279,8.56-110.572,11.281-152.175,10.486-38.193-.731-60.579.248-75,1.228-10.911.74-17.26,1.481-22.442,1.481C-406.146,209.121-410.534,208.161-418.426,205.146Z"
         transform="translate(418.926 -58.86)"
-        fill="#14005a"
+        fill="var(--secondary-bg-color)"
         stroke-width="1"
       />
     </svg>
