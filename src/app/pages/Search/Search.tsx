cecc5ab774ecf8cfef393cd43285565cae615332
Niklas Schäfers <Niklas.Schaefers@gmx.de>
2021-06-15 10:26:55 +0200

    🎨 outsource search object to searchheader

--- src/app/pages/Search/Search.tsx
+++ src/app/pages/Search/Search.tsx
@@ -1,35 +1,70 @@
-import React, { useState } from "react";
+import React from "react";
 import FooterMenu from "../../components/FooterMenu/FooterMenu";
-import SearchField from "../../components/SearchField/SearchField";
-import BubbleHeader from "../../components/Styling Elements/BubbleHeader";
+import HeaderSearch from "../../components/HeaderSearch/HeaderSearch";
 import styles from "./Search.module.css";
 
 function Search(): JSX.Element {
-  const [search, setSearch] = useState("");
-
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    alert(`Search`);
-  }
-
   return (
     <div className={styles.container}>
-      <div className={styles.bubbleHeader}>
-        <BubbleHeader />
+      <div className={styles.headerSearch}>
+        <HeaderSearch />
       </div>
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <SearchField
-          value={search}
-          onChange={setSearch}
-          placeholder="Search..."
-        />
-      </form>
       <div className={styles.result}>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
           dolore est magni odit earum sequi tempore dolorum perspiciatis illum!
-          Repellendus nostrum voluptatem nemo enim nisi saepe quam, molestiae
-          dolorem illo.
+          Repellendoluptatem nemo enim nisi saepe quam, molestiae dolorem illo.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          dolore est magni odit earum sequi tempore dolorum perspiciatis illum!
+          Repellendoluptatem nemo enim nisi saepe quam, molestiae dolorem
+          illo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          dolore est magni odit earum sequi tempore dolorum perspiciatis illum!
+          Repellendoluptatem nemo enim nisi saepe quam, molestiae dolorem
+          illo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          dolore est magni odit earum sequi tempore dolorum perspiciatis illum!
+          Repellendoluptatem nemo enim nisi saepe quam, molestiae dolorem
+          illo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          dolore est magni odit earum sequi tempore dolorum perspiciatis illum!
+          Repellendoluptatem nemo enim nisi saepe quam, molestiae dolorem
+          illo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          dolore est magni odit earum sequi tempore dolorum perspiciatis illum!
+          Repellendoluptatem nemo enim nisi saepe quam, molestiae dolorem
+          illo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          dolore est magni odit earum sequi tempore dolorum perspiciatis illum!
+          Repellendoluptatem nemo enim nisi saepe quam, molestiae dolorem
+          illo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          dolore est magni odit earum sequi tempore dolorum perspiciatis illum!
+          Repellendoluptatem nemo enim nisi saepe quam, molestiae dolorem
+          illo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          dolore est magni odit earum sequi tempore dolorum perspiciatis illum!
+          RepeLorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          dolore est magni odit earum sequi tempore dolorum perspiciatis illum!
+          Repellendoluptatem nemo enim nisi saepe quam, molestiae dolorem illo.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          dolore est magni odit earum sequi tempore dolorum perspiciatis illum!
+          Repellendoluptatem nemo enim nisi saepe quam, molestiae dolorem
+          illo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          dolore est magni odit earum sequi tempore dolorum perspiciatis illum!
+          Repellendoluptatem nemo enim nisi saepe quam, molestiae dolorem
+          illo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          dolore est magni odit earum sequi tempore dolorum perspiciatis illum!
+          Repellendoluptatem nemo enim nisi saepe quam, molestiae dolorem
+          illo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          dolore est magni odit earum sequi tempore dolorum perspiciatis illum!
+          Repellendoluptatem nemo enim nisi saepe quam, molestiae dolorem
+          illo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          dolore est magni odit earum sequi tempore dolorum perspiciatis illum!
+          Repellendoluptatem nemo enim nisi saepe quam, molestiae dolorem
+          illo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          dolore est magni odit earum sequi tempore dolorum perspiciatis illum!
+          Repellendoluptatem nemo enim nisi saepe quam, molestiae dolorem
+          illo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          dolore est magni odit earum sequi tempore dolorum perspiciatis illum!
+          Repellendoluptatem nemo enim nisi saepe quam, molestiae dolorem
+          illo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          dolore est magni odit earum sequi tempore dolorum perspiciatis illum!
+          Repellendoluptatem nemo enim nisi saepe quam, molestiae dolorem
+          illo.llendoluptatem nemo enim nisi saepe quam, molestiae dolorem illo.
         </p>
       </div>
       <div className={styles.footerMenu}>
