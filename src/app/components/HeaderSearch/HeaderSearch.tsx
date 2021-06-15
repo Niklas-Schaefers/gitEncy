944d932f0004cd8550729f9360001b638980eb3a
Niklas Schäfers <Niklas.Schaefers@gmx.de>
2021-06-15 10:24:02 +0200

    💄 insert serachfield headersearch

--- src/app/components/HeaderSearch/HeaderSearch.tsx
+++ src/app/components/HeaderSearch/HeaderSearch.tsx
@@ -1,21 +1,36 @@
-import React from "react";
-import HomeIcon from "../Icons/HomeIcon";
-import SavedDetailsIcon from "../Icons/SavedDetailsIcon";
-import SearchFieldIcon from "../Icons/SearchFieldIcon";
-import BubbleFooter from "../Styling Elements/BubbleFooter";
+import React, { useState } from "react";
+import CatLogo from "../Icons/CatLogo";
+import PlusIcon from "../Icons/PlusIcon";
+import SearchField from "../SearchField/SearchField";
+import BubbleHeader from "../Styling Elements/BubbleHeader";
 import styles from "./HeaderSearch.module.css";
 
 function HeaderSearch(): JSX.Element {
+  const [search, setSearch] = useState("");
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    alert(`Search`);
+  }
+
   return (
-    <div className={styles.footerMenu}>
+    <div className={styles.container}>
       <div className={styles.bubbleHeader}>
-        <BubbleFooter />
+        <BubbleHeader />
       </div>
-      <div className={styles.navBar}>
-        <HomeIcon />
-        <SavedDetailsIcon />
-        <SearchFieldIcon />
+      <div className={styles.catLogo}>
+        <CatLogo />
       </div>
+      <div className={styles.plusIcon}>
+        <PlusIcon />
+      </div>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <SearchField
+          value={search}
+          onChange={setSearch}
+          placeholder="Search..."
+        />
+      </form>
     </div>
   );
 }
