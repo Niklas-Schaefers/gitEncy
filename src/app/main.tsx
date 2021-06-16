2791f7a0e3e8df663e330f3d9c2bfb841db4c80a
Niklas Schäfers <Niklas.Schaefers@gmx.de>
2021-06-16 22:13:43 +0200

    outcomment app tsx

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./globals.css";
-import App from "./App";
+// import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode>{/* <App /> */}</React.StrictMode>,
   document.querySelector("#root")
 );
