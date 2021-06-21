aeed6285f08bf749c50a966e32f4eab6a15178c4
Niklas Schäfers <Niklas.Schaefers@gmx.de>
2021-06-21 09:17:12 +0200

    improve usefetch function for error handling

--- src/app/usefetch.ts
+++ src/app/usefetch.ts
@@ -4,16 +4,21 @@ function useFetch(
   url: string
 ): {
   data: string | null;
+  isLoading: boolean;
+  errorMessage: string | null;
 } {
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [data, setData] = useState<string | null>(null);
+  const isLoading = data === null;
 
   useEffect(() => {
     fetch(url)
       .then((response) => response.text())
-      .then(setData);
+      .then(setData)
+      .catch((error) => setErrorMessage(error.toString()));
   }, []);
 
-  return { data };
+  return { data, isLoading, errorMessage };
 }
 
 export default useFetch;
