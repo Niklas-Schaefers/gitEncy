33f47f844cb25884f71ba071c1ce8fd3c2e117c2
Niklas Schäfers <Niklas.Schaefers@gmx.de>
2021-06-22 17:34:49 +0200

    ✨ Implement database/mongoclient

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,3 +1,24 @@
+import { Collection, MongoClient } from "mongodb";
+import { TransformedResult } from "../types";
+
+let client: MongoClient;
+
 export const connectDatabase = async (url: string): Promise<void> => {
-  console.log(url);
+  client = new MongoClient(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  await client.connect();
+};
+
+export const disconnectDatabase = async (): Promise<void> => {
+  await client.close();
+};
+
+export const getCollection = <T>(name: string): Collection<T> => {
+  return client.db().collection<T>(name);
+};
+
+export const getSearchResultsCollection = (): Collection<TransformedResult> => {
+  return getCollection<TransformedResult>("searchResult");
 };
