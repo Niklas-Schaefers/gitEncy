fdb21d4c829b68308727ce33f2a76c39c93bcc5a
Niklas Schäfers <Niklas.Schaefers@gmx.de>
2021-06-22 10:21:16 +0200

    ✨ HeaderBar for SavedResults page

---
+++ src/app/components/HeaderBar/HeaderBar.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import HeaderBar from "./HeaderBar";
+
+export default {
+  title: "Component/HeaderMenu ",
+  component: HeaderBar,
+};
+
+export const Default = (): JSX.Element => <HeaderBar />;
---
+++ src/app/components/HeaderBar/HeaderBar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import CatLogo from "../Icons/CatLogo";
+import styles from "./HeaderBar.module.css";
+
+function HeaderSearch(): JSX.Element {
+  return (
+    <div className={styles.container}>
+      <div className={styles.catLogo}>
+        <CatLogo />
+      </div>
+      <div className={styles.headline}>
+        <h1>Saved Results</h1>
+      </div>
+    </div>
+  );
+}
+
+export default HeaderSearch;
