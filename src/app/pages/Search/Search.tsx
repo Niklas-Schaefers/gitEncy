5ce321c1d0a23dfd28848f8252c0f2f23a024d81
Niklas Schäfers <Niklas.Schaefers@gmx.de>
2021-06-23 19:01:33 +0200

    🏷️ add types

--- src/app/pages/Search/Search.tsx
+++ src/app/pages/Search/Search.tsx
@@ -5,12 +5,29 @@ import SearchResults from "../../components/SearchResults/SearchResults";
 import styles from "./Search.module.css";
 
 type GitHubData = {
-  items: [{ name: string; html_url: string }];
+  items: GitHubItems[];
+};
+
+type GitHubItems = {
+  name: string;
+  html_url: string;
+  repository: Repository;
+};
+
+type Repository = {
+  full_name: string;
+  owner: Owner;
+};
+
+type Owner = {
+  avatar_url: string;
 };
 
 export type TransformedResult = {
   name: string;
   rawUrl: string;
+  repoName: string;
+  ownerImageUrl: string;
 };
 
 function Search(): JSX.Element {
@@ -20,9 +37,11 @@ function Search(): JSX.Element {
     fetch(`/api/search?code=${searchValue}&user=${filterValue}`)
       .then((response) => response.json())
       .then((data: GitHubData) => {
-        const transformed = data.items.map((item) => {
+        const transformed: TransformedResult[] = data.items.map((item) => {
           return {
             name: item.name,
+            repoName: item.repository.full_name,
+            ownerImageUrl: item.repository.owner.avatar_url,
             rawUrl: item.html_url
               .replace(
                 "https://github.com/",
@@ -41,6 +60,7 @@ function Search(): JSX.Element {
 
   return (
     <div className={styles.container}>
+      {console.log(searchResults)}
       <HeaderSearch onSubmit={fetchGitHubData} />
       <div className={styles.searchElements}>{searchElements}</div>
       <FooterMenu />
