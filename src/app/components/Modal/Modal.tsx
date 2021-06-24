b71363a139e5ed190cba7885857167000faea32b
Niklas Schäfers <Niklas.Schaefers@gmx.de>
2021-06-24 14:35:45 +0200

    ♻️ outsouces naming

--- src/app/components/Modal/Modal.tsx
+++ src/app/components/Modal/Modal.tsx
@@ -13,13 +13,23 @@ type SearchResults = {
 function Modal({ searchResults, setShowModal }: SearchResults): JSX.Element {
   const { data: code } = useFetch(searchResults.rawUrl);
 
+  const userName = searchResults.repoName.substring(
+    0,
+    searchResults.repoName.lastIndexOf("/")
+  );
+  const fileName = searchResults.name.substring(
+    0,
+    searchResults.name.lastIndexOf(".")
+  );
+  const repoName = searchResults.repoName.substring(
+    searchResults.repoName.indexOf("/") + 1
+  );
+
   return (
     <div className={styles.modalWrapper}>
       <div className={styles.modal}>
         <div className={styles.header}>
-          {searchResults.name.substring(0, searchResults.name.lastIndexOf("."))}
-          <br />
-          {searchResults.repoName}
+          {userName} {" - "} {fileName} {" - "} {repoName}
         </div>
         <div className={styles.code}>
           <CodeHighlighted code={code || ""} />
