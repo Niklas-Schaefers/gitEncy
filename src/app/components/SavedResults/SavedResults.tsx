101cb644a5b9ab6fa42b399697ca34bdfd2dca5b
Niklas Schäfers <Niklas.Schaefers@gmx.de>
2021-06-24 14:36:03 +0200

    ✨ implement collapse

--- src/app/components/SavedResults/SavedResults.tsx
+++ src/app/components/SavedResults/SavedResults.tsx
@@ -5,26 +5,56 @@ import { SearchResults } from "../../../types";
 
 function SavedResultsComponent({ searchResult }: SearchResults): JSX.Element {
   const [showModal, setShowModal] = useState<boolean>(false);
+  const [showCollapse, setShowCollapse] = useState(false);
+
+  const userName = searchResult.repoName.substring(
+    0,
+    searchResult.repoName.lastIndexOf("/")
+  );
+  const fileName = searchResult.name.substring(
+    0,
+    searchResult.name.lastIndexOf(".")
+  );
+  const repoName = searchResult.repoName.substring(
+    searchResult.repoName.indexOf("/") + 1
+  );
 
   return (
-    <div className={styles.searchResults}>
-      <div className={styles.results}>
-        <span className={styles.name}>
-          {searchResult.name.substring(0, searchResult.name.lastIndexOf("."))}
-          <br />
-          {searchResult.repoName}
-
-          {showModal && (
-            <Modal
-              searchResults={searchResult}
-              setShowModal={setShowModal}
-            ></Modal>
-          )}
-        </span>
-        <button className={styles.button} onClick={() => setShowModal(true)}>
-          Check Code
-        </button>
-      </div>
+    <div className={styles.collapse}>
+      <button
+        onClick={() => setShowCollapse(!showCollapse)}
+        className={styles.toggle}
+      >
+        {fileName} {" by "}
+        {userName}
+      </button>
+      {showCollapse && (
+        <div>
+          <div className={styles.searchResults}>
+            <img
+              className={styles.ownerImageUrl}
+              src={searchResult.ownerImageUrl}
+            />
+            <span className={styles.fileName}>File: {fileName}</span>
+            <span className={styles.userName}>User: {userName}</span>
+            <span className={styles.repoName}>Repo: {repoName}</span>
+            <span>
+              {showModal && (
+                <Modal
+                  searchResults={searchResult}
+                  setShowModal={setShowModal}
+                ></Modal>
+              )}
+            </span>
+            <button
+              className={styles.button}
+              onClick={() => setShowModal(true)}
+            >
+              Check Code
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/app/pages/Login/Login.tsx
+++ src/app/pages/Login/Login.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import LoginLogo from "../../components/Icons/LoginLogo";
 import LabeledInput from "../../components/LabeledInput/LabeledInput";
+import MainBubble from "../../components/Styling Elements/MainBubble";
 import styles from "./Login.module.css";
 
 function Login(): JSX.Element {
@@ -13,33 +14,39 @@ function Login(): JSX.Element {
   }
 
   return (
-    <div className={styles.container}>
-      <div className={styles.logo}>
-        <LoginLogo />
-      </div>
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <div className={styles.form}>
-          <LabeledInput
-            placeholder="E-Mail"
-            label="email"
-            type="email"
-            value={email}
-            onChange={setEmail}
-            required
-          />
-          <LabeledInput
-            placeholder="Password"
-            label="password"
-            type="password"
-            value={password}
-            onChange={setPassword}
-            required
-          />
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <LoginLogo />
         </div>
-        <Link to="/search" className={styles.buttonLink}>
-          <button className={styles.loginButton}>Log in</button>
-        </Link>
-      </form>
+
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <div className={styles.form}>
+            <LabeledInput
+              placeholder="E-Mail"
+              label="email"
+              type="email"
+              value={email}
+              onChange={setEmail}
+              required
+            />
+            <LabeledInput
+              placeholder="Password"
+              label="password"
+              type="password"
+              value={password}
+              onChange={setPassword}
+              required
+            />
+          </div>
+          <Link to="/search" className={styles.buttonLink}>
+            <button className={styles.loginButton}>Log in</button>
+          </Link>
+        </form>
+      </div>
+      <div className={styles.mainBubble}>
+        <MainBubble />
+      </div>
     </div>
   );
 }
