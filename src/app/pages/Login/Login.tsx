970963f2e72dac0836365f78162ab4ff7ea29054
Niklas Schäfers <Niklas.Schaefers@gmx.de>
2021-06-28 17:49:45 +0200

    ✨ implement github login

--- src/app/pages/Login/Login.tsx
+++ src/app/pages/Login/Login.tsx
@@ -1,48 +1,19 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 import LoginLogo from "../../components/Icons/LoginLogo";
-import LabeledInput from "../../components/LabeledInput/LabeledInput";
 import MainBubble from "../../components/Styling Elements/MainBubble";
+
 import styles from "./Login.module.css";
 
 function Login(): JSX.Element {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-  }
-
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <div className={styles.logo}>
           <LoginLogo />
         </div>
-
-        <form onSubmit={handleSubmit} className={styles.form}>
-          <div className={styles.form}>
-            <LabeledInput
-              placeholder="E-Mail"
-              label="email"
-              type="email"
-              value={email}
-              onChange={setEmail}
-              required
-            />
-            <LabeledInput
-              placeholder="Password"
-              label="password"
-              type="password"
-              value={password}
-              onChange={setPassword}
-              required
-            />
-          </div>
-          <Link to="/search" className={styles.buttonLink}>
-            <button className={styles.loginButton}>Log in</button>
-          </Link>
-        </form>
+        <a className={styles.loginButton} href="/githublogin">
+          Log in with GitHub
+        </a>
       </div>
       <div className={styles.mainBubble}>
         <MainBubble />
--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,76 @@ import path from "path";
 import express from "express";
 import router from "./server/routes";
 import { connectDatabase } from "./utils/database";
-
-const { PORT = 6000 } = process.env;
+import fetch from "node-fetch";
+import cookieSession from "cookie-session";
 
 const app = express();
+app.use(
+  cookieSession({
+    secret: process.env.COOKIE_SECRET,
+  })
+);
+
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+const redircet_uri = process.env.REDIRCET_URI;
+
+console.log({ client_id, client_secret });
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello GitHub auth");
+});
+
+app.get("/githublogin", (_req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=${redircet_uri}`
+  );
+});
+
+async function getAccessToken({ code, client_id, client_secret }) {
+  const res = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      client_id,
+      client_secret,
+      code,
+    }),
+  });
+  const data = await res.json();
+  const params = new URLSearchParams(data);
+  return params.get("access_token");
+}
+async function fetchGitHubUser(token) {
+  const request = await fetch("https://api.github.com/user", {
+    headers: {
+      Authorization: "token " + token,
+    },
+  });
+  return await request.json();
+}
+
+app.get("/login/github/callback", async (req, res) => {
+  const code = req.query.code;
+  const access_token = await getAccessToken({ code, client_id, client_secret });
+  const user = await fetchGitHubUser(access_token);
+  if (user) {
+    req.session.access_token = access_token;
+    req.session.githubId = user.id;
+    res.redirect("/admin");
+  } else {
+    res.send("Login did not succeed!");
+  }
+});
+
+app.get("/logout", (req, res) => {
+  if (req.session) req.session = null;
+  res.redirect("/");
+});
 
 app.use("/api", router);
 
@@ -31,6 +95,8 @@ if (process.env.MONGODB_URL === undefined) {
 
 connectDatabase(process.env.MONGODB_URL).then(() => {
   console.log("Database connected");
+
+  const { PORT = 6000 } = process.env;
   app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}}`);
   });
