3944bfec4bf9034c224428199510e04e9b67df28
Niklas Schäfers <Niklas.Schaefers@gmx.de>
2021-07-02 13:53:16 +0200

    💄 implement logout component

--- src/app/pages/Profile/Profile.tsx
+++ src/app/pages/Profile/Profile.tsx
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth, useUser } from "../../auth/AuthContext";
 import FooterMenu from "../../components/FooterMenu/FooterMenu";
+import CatLogo from "../../components/Icons/CatLogo";
+import CloseIcon from "../../components/Icons/CloseIcon";
 import LogoutIcon from "../../components/Icons/LogoutIcon";
 import styles from "./Profile.module.css";
 
 function Profile(): JSX.Element {
+  const [showLogoutMessage, setShowLogoutMessage] = useState<boolean>(false);
   const { logout } = useAuth();
   const user = useUser();
 
-  function LogOutandMessage() {
-    logout();
-    return alert("Your are logged out now.");
-  }
+  if (showLogoutMessage)
+    return (
+      <div className={styles.logoutMessage}>
+        <CatLogo className={styles.catLogo} />
+        <h1>Logout?</h1>
+        <button className={styles.confirmLogoutButton} onClick={() => logout()}>
+          <LogoutIcon className={styles.confirmLogoutButton__logoutIcon} /> Log
+          out
+        </button>
+        <button
+          className={styles.stayButton}
+          onClick={() => setShowLogoutMessage(false)}
+        >
+          <CloseIcon />
+          No, I want to stay
+        </button>
+      </div>
+    );
 
   return (
     <div className={styles.container}>
-      <button className={styles.logoutButton} onClick={LogOutandMessage}>
+      <button
+        className={styles.logoutButton}
+        onClick={() => setShowLogoutMessage(true)}
+      >
         <LogoutIcon className={styles.logoutIcon} />
       </button>
       <h1 className={styles.userName}>{user.name}</h1>
