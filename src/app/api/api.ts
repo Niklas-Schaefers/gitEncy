66dac5aaaf689cfccc436706acf0f4ae651060f6
Niklas Schäfers <Niklas.Schaefers@gmx.de>
2021-07-07 14:25:22 +0200

    ⚰️ remove unused function

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -57,33 +57,3 @@ export async function deleteSearchResult({
     throw errorMessage;
   }
 }
-
-export async function getSavedSearchResultsForUser({
-  name,
-  ownerImageUrl,
-  rawUrl,
-  repoName,
-  searchValue,
-  id,
-}: TransformedResult): Promise<TransformedResult> {
-  const response = await fetch("/api/searchresults", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      name,
-      ownerImageUrl,
-      rawUrl,
-      repoName,
-      searchValue,
-      id,
-    }),
-  });
-  if (!response.ok) {
-    const errorMessage = await response.text();
-    throw errorMessage;
-  }
-  const result: TransformedResult = await response.json();
-  return result;
-}
